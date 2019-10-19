a1183dd7947a177f71fb05fe3b9f0699b6c34cbb
Craig Bailey <craigbailey.dev@gmail.com>
2019-10-19 22:57:29 +0000

    Added API implementations for sources

--- lambdas/api/index.py
+++ lambdas/api/index.py
@@ -1,4 +1,152 @@
 import boto3
+import os
+import json
+import awsgi
+from botocore.exceptions import ClientError 
+from flask import (
+    Flask, 
+    jsonify, 
+    request, 
+    Response
+)
+from dynamodb_json import json_util as dynamo_json
+
+app = Flask(__name__)
+
+dynamo_client = boto3.client("dynamodb")
+sns_client = boto3.client("sns")
+
+def handle_generic_error(e): 
+    traceback.print_exc() 
+    return Response( 
+        json.dumps({ 
+            "Error" : { 
+                "Message" : str(e), 
+                "Type" : "Internal" 
+            } 
+        }), 
+        status=502 
+    ) 
+ 
+ 
+def handle_boto3_error(ce): 
+    traceback.print_exc() 
+    code = ce.response['Error']['Code'] if 'Code' in ce.response['Error'] else '[Code not found]' 
+    message = ce.response['Error']['Message'] if 'Message' in ce.response['Error'] else '[Message not found]' 
+    err_type = ce.response['Error']['Type'] if 'Type' in ce.response['Error'] else '[Type not found]' 
+    return Response( 
+        json.dumps({ 
+            "Error" : { 
+                "Message" : "{}: {}".format(code, message), 
+                "Type" : err_type 
+            } 
+        }), 
+        status=ce.response['ResponseMetadata']['HTTPStatusCode'] 
+    )
+    
+
+@app.route('/sources', methods=["GET"])
+def list_sources():
+    try: 
+        sources = []
+        next_scan_key = "?"
+        while next_scan_key:
+            scan_request = {
+                "TableName" : os.environ["DYNAMO_TABLE"]
+            }
+            if next_scan_key != "?":
+                request["ExclusiveStartKey"] = next_scan_key
+            scan_response = dynamo_client.scan(**scan_request)
+            if scan_response["Count"] != 0:
+                sources.extend([
+                    dynamo_json.loads(item, as_dict=True)
+                    for item in scan_response['Items']
+                ])
+            next_scan_key = scan_response["LastEvaluatedKey"] if "LastEvaluatedKey" in scan_response else None
+        return sources
+    except ClientError as ce: 
+        return handle_boto3_error(ce) 
+    except Exception as e: 
+        return handle_generic_error(e) 
+
+
+@app.route('/sources', methods=["POST"])
+def create_source():
+    try: 
+        request_body = request.get_json(force=True) 
+        if "source" not in request_body:
+            return jsonify(status=400, message="Missing required property 'source'") 
+        dynamo_client.put_item(
+            TableName=os.environ["DYNAMO_TABLE"],
+            Item={
+                "source" : {
+                    "S" : request_body["source"]
+                }
+            }
+        )
+        return {"Message" : "Success"}
+    except ClientError as ce: 
+        return handle_boto3_error(ce) 
+    except Exception as e: 
+        return handle_generic_error(e)
+
+
+
+@app.route('/sources', methods=["DELETE"])
+def delete_source():
+    try: 
+        request_body = request.get_json(force=True) 
+        if "source" not in request_body:
+            return jsonify(status=400, message="Missing required property 'source'")
+        dynamo_client.delete_item(
+            TableName=os.environ["DYNAMO_TABLE"],
+            Key={
+                "source" : {
+                    "S" : request_body["source"]
+                }
+            }
+        )
+        return {"Message" : "Success"}
+    except ClientError as ce: 
+        return handle_boto3_error(ce) 
+    except Exception as e: 
+        return handle_generic_error(e)
+
+
+@app.route('/subscriptions', methods=["GET"])
+def list_subscriptions():
+    try: 
+        return {"Message" : "Success"}
+    except ClientError as ce: 
+        return handle_boto3_error(ce) 
+    except Exception as e: 
+        return handle_generic_error(e) 
+
+
+@app.route('/subscriptions', methods=["POST"])
+def create_subscription():
+    try: 
+        return {"Message" : "Success"}
+    except ClientError as ce: 
+        return handle_boto3_error(ce) 
+    except Exception as e: 
+        return handle_generic_error(e)
+
+
+@app.route('/subscriptions/<id>', methods=["DELETE"])
+def delete_subscription(id):
+    try: 
+        return {"Message" : "Success"}
+    except ClientError as ce: 
+        return handle_boto3_error(ce) 
+    except Exception as e: 
+        return handle_generic_error(e)
+
 
 def handler(event, context):
-    pass
+    response = awsgi.response(app, event, context) 
+    if "headers" in response: 
+        response["headers"]["Access-Control-Allow-Origin"] = "*" 
+    else: 
+        response["headers"] = { "Access-Control-Allow-Origin" : "*" } 
+    return response 
--- lambdas/list-sources/index.py
+++ lambdas/list-sources/index.py
@@ -1,4 +1,27 @@
 import boto3
+import json
+import os
+import traceback
+
+dynamo_client = boto3.client('dynamodb')
+sqs_client = boto3.client('sqs')
 
 def handler(event, context):
-    pass
+    next_scan_key = "?"
+    while next_scan_key:
+        scan_request = {
+            "TableName" : os.environ["DYNAMO_TABLE"]
+        }
+        if next_scan_key != "?":
+            request["ExclusiveStartKey"] = next_scan_key
+        scan_response = dynamo_client.scan(**scan_request)
+        if scan_response["Count"] != 0:
+            for item in scan_response["Items"]:
+                try:
+                    sqs_client.send_message(
+                        QueueUrl=os.environ["QUEUE_URL"],
+                        MessageBody=item["source"]["S"]
+                    )
+                except:
+                    traceback.print_exc()
+        next_scan_key = scan_response["LastEvaluatedKey"] if "LastEvaluatedKey" in scan_response else None
